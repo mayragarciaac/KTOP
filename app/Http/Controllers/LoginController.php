fd4ab413a65c477dea5b5b7ca332959cfdd3b9f4
Mayra Garcia <mgarcia@axedra.com>
2020-08-29 11:42:38 +0100

    testing login deployment

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -24,6 +24,7 @@ class LoginController extends Controller
     public function logout()
     {
         Auth::logout();
+        Session::forget('user_id');
         return redirect('/login');
     }
 
