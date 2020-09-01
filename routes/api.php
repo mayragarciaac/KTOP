1ccd2307482362d31d07f9e2ab0e07891b048f40
Mayra Garcia <mgarcia@axedra.com>
2020-09-01 23:04:56 +0100

    API

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,11 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+Route::post('login', 'API\UserController@login');
+Route::post('register', 'API\UserController@register');
+Route::group(['middleware' => 'auth:api'], function(){
+    Route::post('details', 'API\UserController@details');
+});
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
